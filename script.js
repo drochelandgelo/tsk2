89e7ce0046ce2017342815e90c70152f447ef9fe
drochelandgelo <116835255+drochelandgelo@users.noreply.github.com>
2023-03-07 14:02:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// taks 1
+
+var admin, name;
+
+name = 'Джон';
+
+admin = name;
+
+alert ( admin );
+
+
+
+// task 2 
+
+let ourPlanetName = 'Earth';
+
+let usserName = "Bill";
+
+//task 3 
+
+
+let num = '10'+10;
+
+console.log(num);
+
+//так работает из за разных типов данных 
